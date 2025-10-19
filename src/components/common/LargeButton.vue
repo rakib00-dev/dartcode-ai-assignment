3507752f500a139f81e044757ab7f151a9d3a240
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-10-19 11:54:48 +0600

    added most of the navabr content in

---
+++ src/components/common/LargeButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+  defineProps({
+    title: String,
+  });
+</script>
+
+<template>
+  <button
+    class="text-sm md:text-md border border-gray-400 rounded-md py-1 px-4 capitalize cursor-pointer"
+  >
+    {{ title }}
+  </button>
+</template>
