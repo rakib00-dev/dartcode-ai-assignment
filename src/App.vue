7c3802806c0ef6500e0d70474d0b53f2cc2ea9f4
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-10-19 19:32:12 +0600

    added new and added two new sections

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+  import ExploreFeatures from "./components/custom/ExploreFeatures.vue";
+  import FeatureBenefits from "./components/custom/FeatureBenefits.vue";
   import Hero from "./components/custom/Hero.vue";
   import Navbar from "./components/custom/Navbar.vue";
 </script>
@@ -6,6 +8,10 @@
 <template>
   <Navbar />
   <Hero />
+  <ExploreFeatures />
+  <FeatureBenefits />
+
+  <div class="mt-80" />
 </template>
 
 <style scoped></style>
