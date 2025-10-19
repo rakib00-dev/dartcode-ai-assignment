8854b63c5cb4518e6ace7778a91b47e8bd4de258
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-10-19 15:48:19 +0600

    updated the hero content

--- src/components/custom/Hero.vue
+++ src/components/custom/Hero.vue
@@ -1,6 +1,7 @@
 <script setup>
   import HeroBgLineEelement from "../common/HeroBgLineEelement.vue";
   import LargeButton from "../common/LargeButton.vue";
+  import { companies } from "../../utils/heroComapnies";
 </script>
 
 <template>
@@ -15,26 +16,44 @@
             >AI-Powered Automation</span
           >
         </h1>
-        <div class="grid gap-8 place-items-center">
-          <div class="relative grid place-items-center">
+        <div class="relative grid gap-8 place-items-center md:px-[20%]">
+          <div class="grid place-items-center">
             <HeroBgLineEelement class="w-full absolute -z-10 max-md:hidden" />
-            <p class="text-center w-1/2">
+            <p class="text-center md:px-5">
               DartCode AI streamlines your QA and release processes by
               automating pull request management, coordinating multi-repository
               operations, and enhancing quality assurance.
             </p>
           </div>
-          <div class="flex justify-center items-center">
+
+          <div class="grid place-items-center gap-[25px] w-full">
             <LargeButton
               -custom-class="bg-[var(--primary-color)] text-white hover:bg-[var(--primary-dark-color)] hover:font-medium px-4"
               padding-x
               title="get started"
             />
+            <span
+              class="w-full bg-[var(--primary-light-color)] text-[var(--primary-color)] text-center p-1"
+            >
+              Explore
+              <a href="#" class="underline leading-[24px] text-[1rem]"
+                >DartCode AI for free.</a
+              ></span
+            >
           </div>
-          <span> Explore <a href="#">DartCode AI for free.</a></span>
         </div>
       </div>
-      <div class=""></div>
+      <div class="grid place-items-center gap-16">
+        <span>Trusted by teams building fast, shipping smart</span>
+        <div class="flex flex-wrap justify-center items-center gap-3 w-full">
+          <div
+            v-for="(svg, i) in companies"
+            :key="`${i}heroCompanyImages`"
+            v-html="svg"
+            class=""
+          />
+        </div>
+      </div>
     </div>
   </section>
 </template>
