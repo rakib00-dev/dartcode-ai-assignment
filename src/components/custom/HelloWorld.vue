dc0f2afcae52e91dd48cbc701d54fedabe1d3ab8
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-10-19 10:07:18 +0600

    updated the vite.config.js

--- src/components/custom/HelloWorld.vue
+++ src/components/custom/HelloWorld.vue
@@ -12,7 +12,9 @@
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button class="border bg-red-800" type="button" @click="count++">
+      count is {{ count }}
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
